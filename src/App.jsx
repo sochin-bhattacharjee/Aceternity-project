3d13856a2257f7cbed5b436114b94960fc5ce6f3
sochin-bhattacharjee <sochin.cs@gmail.com>
2024-11-28 21:57:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { ThreeDCardDemo } from './test'
+import { AnimatedPinDemo } from './test2';
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-4xl'>Hello</h1>
+      <ThreeDCardDemo />
+      <AnimatedPinDemo></AnimatedPinDemo>
+    </div>
+  )
+}
+
+export default App
