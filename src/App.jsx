06ecb82a50ddfeb49c32c235c4034d024031c7ce
sochin-bhattacharjee <sochin.cs@gmail.com>
2024-11-29 10:40:47 +0600

    added some aceternity components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { ThreeDCardDemo } from './test'
 import { AnimatedPinDemo } from './test2';
+import { AnimatedTestimonialsDemo } from './test3';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <h1 className='text-4xl'>Hello</h1>
       <ThreeDCardDemo />
       <AnimatedPinDemo></AnimatedPinDemo>
+      <AnimatedTestimonialsDemo></AnimatedTestimonialsDemo>
     </div>
   )
 }
